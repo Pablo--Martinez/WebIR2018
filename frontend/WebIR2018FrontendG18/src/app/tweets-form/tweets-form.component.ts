02eb0703798ff7474615fe0d8d150e0c0782ed91
Bruno Di Bello <brunodibello@hotmail.com>
2018-10-07 23:40:16 -0300

    Resuelto problema de CrossOrigin y flujo de obtener cantidad de tweets completo

--- frontend/WebIR2018FrontendG18/src/app/tweets-form/tweets-form.component.ts
+++ frontend/WebIR2018FrontendG18/src/app/tweets-form/tweets-form.component.ts
@@ -1,4 +1,8 @@
+import { WebserviceService } from './../webservice.service';
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+
 
 @Component({
   selector: 'app-tweets-form',
@@ -8,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class TweetsFormComponent implements OnInit {
 
-  constructor() { }
+  ocurrencias;
+  palabra;
+
+  constructor(private http: Http, private service: WebserviceService){ 
+  }
 
   ngOnInit() {
+    this.palabra = "";
+    this.ocurrencias=0;
+  }
+
+  buscarTweets(){
+    this.service.getTweetsCount(this.palabra).then((data:any)=>{
+      this.ocurrencias = data;
+    });
   }
 
 }
